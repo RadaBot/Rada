0ab1b08b5e5e8946f0fb30488055756b2a904116
Brys <35676267+brys0@users.noreply.github.com>
2021-08-31 18:48:34 -0500

    Finish basic reminder support

--- src/SlashCommands/Utility/Reminder.js
+++ src/SlashCommands/Utility/Reminder.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { poll } = require('../../../lib/util/constants');
-
+const ms = require('ms');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('reminder')
@@ -20,6 +20,21 @@ module.exports = {
     async execute(interaction, client) {
         let text = interaction.options.getString('text');
         let duration = interaction.options.getString('duration');
-        
+        let current = ms(ms(duration), { long: true });
+        if (text.length > 800) {
+            return await interaction.reply({
+                content: `**Reminder message must be less than 800 charecters**`,
+                ephemeral: true
+            });
+        }
+        let embed = client.util.embed()
+            .setColor(client.misc.color)
+            .setTitle('Reminder')
+        let date = new Date(Date.now() + ms(duration));
+        await interaction.reply({
+            content: `I will remind you in \`${current}\``
+        })
+        const message = await interaction.fetchReply()
+        await client.RadaReminder.create(date, message, interaction.user, text, embed, current);
     }
 };
