a2e1a68fc1fc9f8d82abc4fcd14832bab7b26520
Brad <44570552+br4dw@users.noreply.github.com>
2023-02-28 16:13:22 +0000

    feat(SlashHandler): dev mode

--- src/handlers/SlashHandler.js
+++ src/handlers/SlashHandler.js
@@ -1,30 +1,31 @@
-const { Routes } = require('discord-api-types/v9');
-const { REST } = require('@discordjs/rest');
-const { readdirSync, statSync } = require('fs');
-const path = require('path');
-require('dotenv').config();
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
-const { production, devGuild } = require('../config');
+const { Routes } = require("discord-api-types/v9")
+const { readdirSync, statSync } = require("fs")
+const { REST } = require("@discordjs/rest")
+const path = require("path")
+require("dotenv").config()
+
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN)
+const { production, devGuild } = require("../config")
 
 class SlashHandler {
     constructor(client, {
         directory
     }) {
-        this.client = client;
-        this.commands = [];
-        this.directory = directory;
+        this.client = client
+        this.commands = []
+        this.directory = directory
     }
 
     async init() {
-        await this.loadCommands();
-        this.client.classLoader.push('[ClassLoader] SlashHandler loaded');
+        await this.loadCommands()
+        // this.client.classLoader.push("[ClassLoader] SlashHandler loaded")
     }
     formatCommands() {
         let formatted = [];
-        const categories = readdirSync(this.directory);
+        const categories = readdirSync(this.directory)
         for (const category of categories) {
-            let commands = this.getCommands(path.join(this.directory, category).split(/\\/g).join('/'));
-            let commandData = [];
+            let commands = this.getCommands(path.join(this.directory, category).split(/\\/g).join('/'))
+            let commandData = []
             for (const command of commands) {
                 let data = require(`${process.cwd()}/${command}`)
                 commandData.push({
@@ -34,9 +35,9 @@ class SlashHandler {
                     ownerOnly: data.ownerOnly || false
                 })
             }
-            formatted.push({ category: category, commands: commandData });
+            formatted.push({ category: category, commands: commandData })
         }
-        return formatted;
+        return formatted
     }
     getCommands(directory) {
         const results = [];
@@ -57,29 +58,31 @@ class SlashHandler {
     async loadCommands() {
         let files = this.getCommands(this.directory);
         for (let file of files) {
-            const command = require(`${process.cwd()}/${file}`);
-            this.commands.push(command.data.toJSON());
-            this.client.slashCommands.set(command.data.name, command);
+            const command = require(`${process.cwd()}/${file}`)
+            this.commands.push(command.data.toJSON())
+            this.client.slashCommands.set(command.data.name, command)
         }
         try {
             if (production) {
+                this.client.classLoader.push("[ClassLoader] SlashHandler loaded in production mode")
                 return await rest.put(Routes.applicationCommands(this.client.user.id), {
                     body: this.commands
-                });
+                })
             } else {
+                let commands = false
+                this.client.classLoader.push(`[ClassLoader] SlashHandler loaded in dev mode with commands ${commands ? "ENABLED" : "DISABLED"}`)
                 return await rest.put(Routes.applicationGuildCommands(this.client.user.id, devGuild), {
-                    // body: this.commands
-                    body: []
-                });
+                    body: commands ? this.commands : []
+                })
             }
         } catch (error) {
             this.client.logger.error([
                 'Loading slash commands failed',
                 `Type:   ${error.name}`,
                 `Error:  ${error.message}`
-            ]);
+            ])
         }
     }
 }
 
-module.exports = SlashHandler;
+module.exports = SlashHandler
